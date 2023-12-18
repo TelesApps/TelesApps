44d0dae16b2488afa2f0211e58af512537648d30
Claudio Teles <telesapps85@gmail.com>
2023-12-18 07:47:52 -0500

    contact us mobile ui fix

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { AnimationService } from '../services/animation.service';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, MatIconModule],
+  imports: [CommonModule, NgOptimizedImage, MatIconModule, MatButtonModule],
   templateUrl: './contact-page.component.html',
   styleUrls: ['../front-end-page/front-end-page.component.scss', './contact-page.component.scss']
 })
